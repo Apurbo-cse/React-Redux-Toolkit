a01c37915b13cce2ac2f55e96fc889e55561aa6e
Apurbo Kumar Anup <apurboka@gmail.com>
2022-12-26 21:52:43 +0600

    Update Post.js

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
 import AddPostForm from "../../features/posts/AddPostForm";
 import PostsList from "../../features/posts/PostsList";
+import PostCard from "../common/PostCard";
 
 const Post = () => {
   return (
     <>
       <div className="container">
         <div className="row my-3">
-          
+          <PostCard />
+
           <PostsList />
 
           <AddPostForm />
