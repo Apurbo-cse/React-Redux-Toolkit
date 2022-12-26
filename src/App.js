a1d50e6de4b3bf5702535b07a9395ef61161ea54
Apurbo Kumar Anup <apurboka@gmail.com>
2022-12-26 13:59:09 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Navbar } from "./components/include/Navbar";
+import Home from "./components/Home";
+import Post from "./components/post/Post";
 import Counter from "./features/counter/Counter";
 
 function App() {
   return (
-    <main className="App">
-      <Counter />
-    </main>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/post" element={<Post />} />
+        <Route exact path="/count" element={<Counter />} />
+      </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./App.css";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import reportWebVitals from "./reportWebVitals";
+
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
