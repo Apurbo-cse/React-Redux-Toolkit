49e6f68a61c01246cedec1ddf43bed110e64bf23
Apurbo Kumar Anup <apurboka@gmail.com>
2022-12-26 21:46:45 +0600

    Update PostCard.js

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -1,25 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Pic1 from "../../assets/images/3.png";
+import PostAuthor from "../../features/posts/PostAuthor";
+import { selectAllPosts } from "../../features/posts/postsSlice";
+import TimeAgo from "../../features/posts/TimeAgo";
+
 const PostCard = () => {
+  const posts = useSelector(selectAllPosts);
   return (
     <>
-      <div className="col-6 col-md-4 col-lg-3 mb-3">
-        <div className="card border-0 shadow text-center  ">
-          <img src={Pic1} className="card-img-top p-2" alt={Pic1} />
-          <div className="card-body">
-            <h5 className="card-title mb-0">
-              {/* {props.title.substring(0, 20)} */}
-              ... Lorem ipsum
-            </h5>
-            <p className="card-text lead fw-bold">10</p>
-            <Link to="#" className="btn btn-outline-dark">
-              Buy Now
-            </Link>
+      {posts.map((post) => (
+        <div className="col-6 col-md-4 col-lg-3 mb-3" key={post.id}>
+          <div className="card border-0 shadow   ">
+            <img src={Pic1} className="card-img-top p-2" alt={Pic1} />
+            <div className="d-flex">
+              <PostAuthor userId={post.userId} />
+              <TimeAgo timestamp={post.date} />
+            </div>
+            <div className="card-body text-center">
+              <h5 className="card-title mb-0">{post.title.substring(0, 20)}</h5>
+              <h6 className="card-title mb-0">
+                {post.content.substring(0, 20)}
+              </h6>
+            </div>
           </div>
         </div>
-      </div>
-      1
+      ))}
     </>
   );
 };
