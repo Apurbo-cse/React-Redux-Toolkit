14dcca6461529cf0c5f47a282a8cdc06b124587d
Apurbo Kumar Anup <apurboka@gmail.com>
2022-12-28 00:27:10 +0600

    Update

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -1,16 +1,17 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Pic1 from "../../assets/images/3.png";
-import PostAuthor from "./PostAuthor";
+// import Pic1 from "../../assets/images/3.png";
+// import PostAuthor from "./PostAuthor";
 import {
   selectAllPosts,
   getPostsStatus,
   getPostsError,
   fetchPosts,
 } from "../../features/posts/postsSlice";
+import PostsExport from "../post/PostsExport";
 
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+// import TimeAgo from "./TimeAgo";
+// import ReactionButtons from "./ReactionButtons";
 
 const PostCard = () => {
   const dispatch = useDispatch();
@@ -27,26 +28,21 @@ const PostCard = () => {
 
   return (
     <>
-      {posts.map((post) => (
-        <div className="col-6 col-md-4 col-lg-3 mb-3" key={post.id}>
-          <div className="card border-0 shadow  p-2 ">
-            <img src={Pic1} className="card-img-top " alt={Pic1} />
-            <div className="d-flex justify-content-between">
-              <div className="col-6">
-                <PostAuthor userId={post.userId} />
-              </div>
-              <div className="col-6 d-flex justify-content-end">
-                <TimeAgo timestamp={post.date} />
-              </div>
-              <hr />
-            </div>
-
-            <h5 className="card-title mb-0">{post.title}</h5>
-            <p className="card-title mb-0">{post.content.substring(0, 35)}..</p>
-            <ReactionButtons post={post} />
-          </div>
-        </div>
-      ))}
+      {postsStatus === "loading" ? (
+        <p>"Loading..."</p>
+      ) : (
+        <>
+          {postsStatus === "failed" ? (
+            <p>{error}</p>
+          ) : (
+            <>
+              {posts.map((post) => (
+                <PostsExport key={post.id} post={post} />
+              ))}
+            </>
+          )}
+        </>
+      )}
     </>
   );
 };
--- src/components/post/PostsExport.js
+++ src/components/post/PostsExport.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Pic1 from "../../assets/images/3.png";
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import PostAuthor from "../common/PostAuthor";
+import ReactionButtons from "../common/ReactionButtons";
+import TimeAgo from "../common/TimeAgo";
 
-const PostsExport = (posts) => {
+const PostsExport = ({ post }) => {
   return (
     <>
       <div className="col-6 col-md-4 col-lg-3 mb-3">
@@ -12,17 +12,17 @@ const PostsExport = (posts) => {
           <img src={Pic1} className="card-img-top " alt={Pic1} />
           <div className="d-flex justify-content-between">
             <div className="col-6">
-              <PostAuthor userId={posts.userId} />
+              <PostAuthor userId={post.userId} />
             </div>
             <div className="col-6 d-flex justify-content-end">
-              <TimeAgo timestamp={posts.date} />
+              <TimeAgo timestamp={post.date} />
             </div>
             <hr />
           </div>
 
-          <h5 className="card-title mb-0">{posts.title}</h5>
-          <p className="card-title mb-0">{posts.content.substring(0, 35)}..</p>
-          <ReactionButtons post={posts} />
+          <h5 className="card-title mb-0">{post.title}</h5>
+          <p className="card-title mb-0">{post.body.substring(0, 35)}..</p>
+          <ReactionButtons post={post} />
         </div>
       </div>
     </>
